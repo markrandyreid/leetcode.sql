bd5239d406762850d92d8e7825d65099d7670dcb
Mark Randy Reid <reid.233@buckeyemail.osu.edu>
2023-12-09 09:01:45 -0800

    Time: 754 ms (97.35%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# filtering table based on specific value and the absense of value
     select  name
       from  Customer
      where  referee_id IS NULL or referee_id != 2;
